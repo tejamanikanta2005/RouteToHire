a2d58f906f172c61ca8dae98e1750cfb7bea5e5b
Teja Manikanta <tejamanikanta54@gmail.com>
2026-07-24 23:39:36 +0530

    Use Streamlit secrets for API key

--- helper.py
+++ helper.py
@@ -1,22 +1,13 @@
-import os
 import streamlit as st
 from google import genai
 
-# Get API key from Streamlit Secrets or .env
-api_key = None
-
-try:
-    api_key = st.secrets["GOOGLE_API_KEY"]
-except:
-    api_key = os.getenv("GOOGLE_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
+client = genai.Client(
+    api_key=st.secrets["GOOGLE_API_KEY"]
+)
 
 def ask_gemini(prompt):
     response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
+        model="gemini-2.5-flash",
         contents=prompt
     )
-
     return response.text
